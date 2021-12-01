4fc554341c1c363e8a56a894c0057db35708ec46
Yaramasu12 <89273452+Yaramasu12@users.noreply.github.com>
2021-11-30 21:46:50 -0600

    pushing

--- models/animal.js
+++ models/animal.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 const animalSchema = mongoose.Schema({
-    count: Number,
-    location: String,
-    name: String
+    count: {type:Number,min:1,max:3000},
+    location: {type:String,length:8},
+    name: String  
+    
 })
 module.exports = mongoose.model("Animal",
     animalSchema)
--- routes/animal.js
+++ routes/animal.js
@@ -1,16 +1,25 @@
 var express = require('express');
 const animal_controlers= require('../controllers/animal');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 /* GET animal */
 router.get('/', animal_controlers.animal_view_all_Page );
 /* GET detail animal page */
 router.get('/detail', animal_controlers.animal_view_one_Page);
 /* GET create animal page */
-router.get('/create', animal_controlers.animal_create_Page);
+router.get('/create', secured, animal_controlers.animal_create_Page);
 /* GET create update page */
-router.get('/update', animal_controlers.animal_update_Page);
+// router.get('/update', animal_controlers.animal_update_Page);
+/* GET update animal page */
+router.get('/update', secured, animal_controlers.animal_update_Page);
 /* GET create animal page */
-router.get('/delete', animal_controlers.animal_delete_Page);
+router.get('/delete', secured, animal_controlers.animal_delete_Page);
 
 
 module.exports = router;
