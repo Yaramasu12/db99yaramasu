43fc403c283b8092cdd1d14fdbd9bc33cd37a59d
Yaramasu12 <89273452+Yaramasu12@users.noreply.github.com>
2021-12-01 00:13:43 -0600

    addede code

--- app.js
+++ app.js
@@ -91,17 +91,6 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/animal', animalRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
-app.use('/', resourceRouter);
-var passport = require('passport'); 
-var LocalStrategy = require('passport-local').Strategy;
-
 passport.use(new LocalStrategy( 
   function(username, password, done) { 
     Account.findOne({ username: username }, function (err, user) { 
@@ -129,6 +118,17 @@ var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate())); 
 passport.serializeUser(Account.serializeUser()); 
 passport.deserializeUser(Account.deserializeUser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/animal', animalRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+app.use('/', resourceRouter);
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
